513e518abb5886df463afd2d728a29affc2f9c8a
michael eze <Michael.eze@ext.elli.eco>
2020-09-28 16:00:30 +0200

    fix

--- src/index.helmet.tsx
+++ src/index.helmet.tsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet";
+import {Helmet} from "react-helmet";
 import App from "./App";
 import React from "react";
 
@@ -7,8 +7,10 @@ const ReactApp = () => (
         <Helmet>
             <meta charSet="utf-8"/>
             <meta name="author" content="Michael Eze <https://linkedin.com/in/michaeleze>"/>
-            <meta title="Michael Eze, Eze, Michael, Software engineer, frontend, portfolio, developer, typescript, React, node, full-stack, UI, UX" />
-            <meta name="description" content="Michael Eze is a passionate Software Engineer | FrontEnd and full-stack React developer also skilled in UI designing based in Germany. Hobbies are programming, dancing, music, cooking and sports(basket ball and football)"/>
+            <meta
+                title="Michael Eze, Eze, Michael, Software engineer, frontend, portfolio, developer, typescript, React, node, full-stack, UI, UX"/>
+            <meta name="description"
+                  content="Michael Eze is a passionate Software Engineer | FrontEnd and full-stack React developer also skilled in UI designing based in Germany. Hobbies are programming, dancing, music, cooking and sports(basket ball and football)"/>
             <link rel="canonical" href="https://michaeleze.de"/>
             <link rel="publisher" href="https://linkedin.com/in/michaeleze"/>
             <meta property="og:locale" content="en_US"/>
@@ -25,11 +27,12 @@ const ReactApp = () => (
             <meta name="linkedin:title" content="Michael Eze &mdash; Germany Based Full-Stack - Node React Engineer"/>
             <meta name="github:site" content="https://github.com/michaeleze"/>
             <meta name="github:title" content="Michael Eze &mdash; Germany Based Full-Stack - Node React Engineer"/>
-            <meta name="medium:site" content="https://medium.com/@michael.eze" />
+            <meta name="medium:site" content="https://medium.com/@michael.eze"/>
             <meta name="medium:title" content="Michael Eze &mdash; Germany Based Full-Stack - Node React Engineer"/>
-            <meta name="linkedin:description"  content="Michael Eze is a passionate Software Engineer <FrontEnd /> and Full-Stack - React Node Engineer also skilled in UX designing based in Germany. Hobbies are programming, dancing, music, cooking and sports(basket ball and football)"/>
-          <meta name="linkedin:image" content="https://github.com/michaeleze/portfolio/blob/master/public/me.jpg" />
-          <link rel="icon" href="https://github.com/michaeleze/portfolio/blob/master/public/me.jpg" />
+            <meta name="linkedin:description"
+                  content="Michael Eze is a passionate Software Engineer <FrontEnd /> and Full-Stack - React Node Engineer also skilled in UX designing based in Germany. Hobbies are programming, dancing, music, cooking and sports(basket ball and football)"/>
+            <meta name="linkedin:image" content="https://github.com/michaeleze/portfolio/blob/master/public/me.jpg"/>
+            <link rel="icon" href="https://github.com/michaeleze/portfolio/blob/master/public/me.jpg"/>
             <meta name="viewport"
                   content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes"/>
             <meta name="theme-color" content="#000000"/>
@@ -37,7 +40,7 @@ const ReactApp = () => (
             <link rel="apple-touch-icon" href="https://github.com/michaeleze/portfolio/blob/master/public/me.jpg"/>
             <title> Michael Eze </title>
         </Helmet>
-        <App />
+        <App/>
     </>
 );
 
